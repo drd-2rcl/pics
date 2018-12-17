ab0f232d3dfd73fa8ceb75f800eb3164519ac607
drd-2rcl <drd.rclrd@gmail.com>
2018-12-17 08:46:15 -0200

    :alembic: Integrated API pics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
 import SearchBar from './components/SearchBar';
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    Axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID e0d66c1a05602fb01d19d7233dda0a815b081af0efcf32efc0c639a0ad8a82b1'
+      }
+    });
   }
 
   render() {
